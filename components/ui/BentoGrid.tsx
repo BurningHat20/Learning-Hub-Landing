00ad2aaad5c8c07f41ce3f4571c8a24d31893cfe
BurningHat20 <yashgohel395@gmail.com>
2024-07-21 16:30:27 +0530

    chore: Update email address for copy functionality in BentoGrid component

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "info@thelearninghub.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
